50d83400c69e5f31ab6af8e210581f03cf9dd5ab
Brandon Schlack <brandonschlack@gmail.com>
2020-07-29 09:51:02 -0700

    Add LAYOUT_all to Radpad

--- keyboards/keybage/radpad/radpad.h
+++ keyboards/keybage/radpad/radpad.h
@@ -28,8 +28,23 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
+#define LAYOUT_all( \
+    k00, k01,      k03, \
+    k10, k11, k12, k13, \
+    k20, k21, k22, k23, \
+    k30, k31, k32, k33, \
+    k40, k41, k42, k43  \
+) \
+{ \
+    { k00, k01, XXX, k03 }, \
+    { k10, k11, k12, k13 }, \
+    { k20, k21, k22, k23 }, \
+    { k30, k31, k32, k33 }, \
+    { k40, k41, k42, k43 }  \
+}
+
 #define LAYOUT_4x4_encoders( \
-    k00,           k03, \
+    k00, k01,      k03, \
     k10, k11, k12, k13, \
     k20, k21, k22, k23, \
     k30, k31, k32, k33, \
